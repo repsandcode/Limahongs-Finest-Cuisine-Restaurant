3245c79187b8f27a4fe07db3ce83daee9bb0104e
Albert Jonathan <borjajonathann@gmail.com>
2022-04-22 20:34:58 +0800

    done with menu section

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// const menu1 = document.querySelector('#menu-one-hover')
+// const menu2 = document.querySelector('#menu-2-hover')
+// const menu3 = document.querySelector('#menu-3-hover')
+// const menu4 = document.querySelector('#menu-4-hover')
+// const menu5 = document.querySelector('#menu-5-hover')
+// const menu6 = document.querySelector('#menu-6-hover')
+// const menu7 = document.querySelector('#menu-7-hover')
+// const menu8 = document.querySelector('#menu-8-hover')
+
+// document.getElementById('menu-one').addEventListener('mouseover', hoverMenu)
+// // document.querySelector('#claireNext').addEventListener('click', claireNext)
+// // document.querySelector('#sharleenNext').addEventListener('click', sharleenNext)
+
+// // .addEventListener("mouseover", function( event )
+// function hoverMenu(){
+// 	menu1.classList.add('hidden')
+// }
